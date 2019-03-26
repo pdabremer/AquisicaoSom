9c9f41d9bea231d06c7021ad9d0280729cae8664
pdabremer <43387282+pdabremer@users.noreply.github.com>
2019-03-26 18:08:11 -0300

    Add files via upload

---
+++ AquisicaoSom/build-robo/CMakeFiles/gravacao.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/media/pedro/DATA/Faculdade/RINOBOT/Nao/Alsa/AquisicaoSom/gravacao.c" "/media/pedro/DATA/Faculdade/RINOBOT/Nao/Alsa/AquisicaoSom/build-robo/CMakeFiles/gravacao.dir/gravacao.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/pedro/Nao/Mari-master/sdk/ctc-linux64-atom-2.1.4.13/alsa/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AquisicaoSom/build-robo/CMakeFiles/gravacao.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gravacao.dir/gravacao.c.o"
+  "sdk/bin/gravacao.pdb"
+  "sdk/bin/gravacao"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gravacao.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
